f4f3980e3b110bc8b24d4fe3d131ab542b0ecfa2
Jesse Lawrence <soterialawrence009@gmail.com>
2024-07-10 03:35:04 +0300

    reworked the links animation

--- manukatto/components/hero.tsx
+++ manukatto/components/hero.tsx
@@ -1,21 +1,32 @@
 
 "use client"
 
-
+//utilities for keeping track of state, variablesAndExternalEvents(usestate), animation(motion), 
+//page to page navigation(Link), icons(Fix, BsBorderWidth)
 import { useState, useEffect } from "react"
 import { motion } from "framer-motion"
 import Link from "next/link"
 import { FiX } from "react-icons/fi"
 import { BsBorderWidth } from "react-icons/bs"
 
+
+
+//TODO: make the hero text solid with a translucent background
+//give the background a bit more height and width 
+//make the background have a gradient
+
+//component declaration
 export default function Herosection() {
 	const [height, setHeight] = useState(window.innerHeight * 0.75)
+	//opening and closing the menu bar for links to pages.
 	const [menuOpen, setMenuOpen] = useState(false)
+	//height for the component
 	const minHeight = window.innerHeight * 0.07
 	const maxHeight = window.innerHeight * 0.75
 
 	function handleScroll() {
 		const scrollPos = window.scrollY
+		setMenuOpen(false)
 		if (scrollPos > minHeight) {
 			setHeight(minHeight)
 		} else {
@@ -32,6 +43,7 @@ export default function Herosection() {
 		}
 	}, [])
 
+	//variants for animation of the menu window.(framer motion utility)
 	const list = {
 		visible: {
 			opacity: 1,
@@ -46,7 +58,7 @@ export default function Herosection() {
 
 	const item = {
 		visible: { opacity: 1, x: 0 },
-		hidden: { opacity: 0, x: -30 },
+		hidden: { opacity: 0, x: -50 },
 	}
 
 	return (
@@ -56,37 +68,43 @@ export default function Herosection() {
 			animate={{ height }}
 			transition={{ duration: 0.5 }}
 		>
-			<div className="flex justify-between items-center px-4">
+			<div className="flex justify-between items-center content-center px-4">
 				<h1 className="w-11/12">Manukatto</h1>
-				<button onClick={() => setMenuOpen(!menuOpen)} className="text-3xl">
+				<button onClick={() => setMenuOpen(!menuOpen)} className="text-3xl z-20">
 					{menuOpen ? <FiX /> : <BsBorderWidth />}
 				</button>
 			</div>
-			<motion.ul
-				initial="hidden"
-				animate={menuOpen ? "visible" : "hidden"}
-				variants={list}
-				className={`fixed top-0 right-0 h-full w-2/3 bg-customWhite flex flex-col justify-center transition-transform duration-500 ${menuOpen ? 'translate-x-0' : 'translate-x-full'
-					}`}
+			<motion.div
+				initial={{ x: '100%' }}
+				animate={{ x: menuOpen ? '0%' : '100%' }}
+				transition={{ type: "spring", stiffness: 300, damping: 30 }}
+				className="fixed top-0 right-0 h-full w-2/3 bg-customWhite flex flex-col justify-center"
 			>
-				<Link href={"/services"}>
-					<motion.li variants={item} className="text-customLight text-center py-1.5">
-						Services
-					</motion.li>
-				</Link>
-				<Link href={"/about_us"}>
-					<motion.li variants={item} className="text-customLight text-center py-1.5">
-						About Us
-					</motion.li>
-				</Link>
-				<Link href={"/booking"}>
-					<motion.li variants={item} className="text-customLight text-center py-1.5">
-						Bookings
-					</motion.li>
-				</Link>
-			</motion.ul>
-			<div className="flex flex-col justify-center w-11/12 mx-auto">
-				<p className="bg-customWhite">Crafting beauty, one tip at a time</p>
+				<motion.ul
+					initial="hidden"
+					animate={menuOpen ? "visible" : "hidden"}
+					variants={list}
+					className="flex flex-col"
+				>
+					<Link href={"/services"}>
+						<motion.li variants={item} className="text-customLight text-center py-1.5">
+							Services
+						</motion.li>
+					</Link>
+					<Link href={"/about_us"}>
+						<motion.li variants={item} className="text-customLight text-center py-1.5">
+							About Us
+						</motion.li>
+					</Link>
+					<Link href={"/booking"}>
+						<motion.li variants={item} className="text-customLight text-center py-1.5">
+							Bookings
+						</motion.li>
+					</Link>
+				</motion.ul>
+			</motion.div>
+			<div className="flex flex-col self-stretch pt-28 pl-8 justify-center w-11/12 mx-auto">
+				<p className="bg-customLight opacity-75">Crafting beauty, one tip at a time</p>
 			</div>
 		</motion.section>
 	)
